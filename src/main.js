7f8a9e7bbcf32279c9fcd0cb22fb0843c801ec60
Student <example@example.com>
2019-09-13 16:22:52 -0700

    Reveal commented out import calls; $ from jquery, bootstrap, bootstrap min, and styles.css

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import {createHTML} from "./triageApp2.js"
-import {array} from "./triageApp2.js"
+// import {createHTML} from "./triageApp2.js"
+// import {array} from "./triageApp2.js"
