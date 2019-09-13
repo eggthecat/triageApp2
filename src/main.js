0d057a8dec5dc55acaf7a40f876c7795d6bb0db3
Student <example@example.com>
2019-09-13 15:26:56 -0700

    add import

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+import {createHTML} from "./triageApp2.js"
+import {array} from "./triageApp2.js"
